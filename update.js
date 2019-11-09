643222f6bb92183b9e1da36b92c6eb9db317bfe3
wizArD-1910 <aman.d@ahduni.edu.in>
2019-11-09 22:09:06 +0530

    resolving update.js

--- update.js
+++ update.js
@@ -14,7 +14,7 @@ export async function main(event, context) {
     },
     // 'UpdateExpression' defines the attributes to be updated
     // 'ExpressionAttributeValues' defines the value in the update expression
-    UpdateExpression: "SET content = :content, attachment = :attachment",
+    UpdateExpression: "SET content = :content, attachment = :attachment, color = :color",
     ExpressionAttributeValues: {
       ":attachment": data.attachment || null,
       ":content": data.content || null,
